d958474d2fde08de7e7d77f748eebd0c9f09b107
minch <mincho.tanev@fdiba.tu-sofia.bg>
2025-08-08 19:14:11 +0300

    add current progress

--- app/core/models/messages.py
+++ app/core/models/messages.py
@@ -4,7 +4,7 @@ from .chats import Chat
 
 class Message(models.Model):
     id = models.AutoField(primary_key=True)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    chats = models.ForeignKey(Chat, on_delete=models.CASCADE, default=0)
     content = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)
