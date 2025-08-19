5af4ea4105b99d79569be09be2376f3f9d370d17
minch <mincho.tanev@fdiba.tu-sofia.bg>
2025-08-20 00:18:19 +0300

    add clientManager/REST_interface

---
+++ services/ClientManager.py
@@ -0,0 +1,38 @@
+from abc import abstractmethod
+import joblib
+import pandas as pd
+import REST_Interface as rest
+
+
+class ClientManager:
+    def __init__(self, database_url=None):
+        self.database_url=database_url
+        self.rest=rest()
+    
+
+    def check_for_new_clients(self):
+        all_clients=self.rest.get_all_customers()
+        # filter the ones who don't have a risk score
+        new_clients= []
+        return new_clients
+
+    def predict_credit_risk(self, client):
+        pipeline = joblib.load("ml/output/credit_risk_pipeline.joblib")
+        client_df = self.generate_client_dataframe(client)
+        is_eligible = True if pipeline.predict_proba(client_df)[0, 1] > 0.5 else False
+        return is_eligible
+
+    @abstractmethod
+    def generate_client_dataframe(client):
+        # Prepare a dataframe with the same columns as training AFTER applying the same feature engineering.
+        # If feature engineering was done outside the pipeline, ensure to replicate it here before calling predict/predict_proba.
+        df_new = pd.DataFrame([{
+            "Age": 35, "Sex": "male", "Job": 2, "Housing": "own",
+            "Saving accounts": "moderate", "Checking account": "moderate",
+            "Credit amount": 3000, "Duration": 18, "Purpose": "car",
+            "Credit_per_month": 3000/18,
+            "Age_group": "Adult",
+            "Credit_amount_category": "Medium",
+            "Duration_category": "Medium"
+        }])
+
---
+++ services/REST_Interface.py
@@ -0,0 +1,26 @@
+import requests
+
+class REST:
+
+    def __init__(self, base_url="http://localhost", port="8000", api_suffix="api"):
+        self.url=f"{base_url}:{port}/{api_suffix}/"
+        self.headers = {
+            "Content-Type": "application/json"
+        }
+
+    def get_all_customers(self):
+        url = f"{self.url}customers/"
+        response = requests.get(url, headers=self.headers)
+        print(response)
+
+    def get_customer_by_id(self, id):
+        url = f"{self.url}customers/{id}/"
+        response = requests.get(url, headers=self.headers)
+        print(response)
+
+    def add_risk_score_to_customer(self, customer_id, new_risk_score):
+        url = f"{self.url}customers/{customer_id}/"
+        response = requests.patch(url, headers=self.headers, json={"risk_score": new_risk_score})
+        print(response)
+
+    
